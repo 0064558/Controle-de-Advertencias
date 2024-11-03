05ee96910eaeed8d73fef99ebaec5344e79eecd8
Rodrigo Alexandre <93837586+0064558@users.noreply.github.com>
2024-11-03 14:10:59 -0300

    feat: show/hide password

--- Controle-de-Advertencias-main/index.php
+++ Controle-de-Advertencias-main/index.php
@@ -1,10 +1,5 @@
-<?php
-    include("php/conexao.php")
-?>
-
 <!DOCTYPE html>
 <html lang="pt-br">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -16,22 +11,33 @@
 <body>
     <form action="php/login.php" method="POST">
         <div class="login-container">
-
             <?php if (!empty($flash)): ?>
                 <p class="flashError"><?= $flash ?></p>
             <?php endif; ?>
 
-            <img class ="Imagem-Login" src="Icons/logo-gradiente.svg" alt="Imagem-Login">
+            <img class="Imagem-Login" src="Icons/logo-gradiente.svg" alt="Imagem-Login">
             <h2>Login</h2>
             <label for="email">Email:</label>
             <input type="email" name="email" required>
+            
             <label for="senha">Senha:</label>
-            <input type="password" name="senha" required>
+            <input type="password" name="senha" id="senha" required>
+            
+            <!-- Checkbox para mostrar senha -->
+            <label>
+                <input type="checkbox" onclick="toggleSenha()"> Mostrar Senha
+            </label>
+
             <button type="submit" id="btn-login">Entrar</button>
         </div>
-
-
     </form>
-</body>
 
+    <!-- Script JavaScript para alternar senha -->
+    <script>
+        function toggleSenha() {
+            const senhaInput = document.getElementById("senha");
+            senhaInput.type = senhaInput.type === "password" ? "text" : "password";
+        }
+    </script>
+</body>
 </html>
